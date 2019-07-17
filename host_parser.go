6bcf9239e5fb853b199305c524980f324b586c37
arr-dev <nenad.petronijevic86@gmail.com>
2019-07-17 15:25:43 +0200

    Add facebook support to parser

--- host_parser.go
+++ host_parser.go
@@ -22,6 +22,10 @@ func parseHost(u *url.URL) *Host {
 	case "readcomiconline.to", "kimcartoon.to":
 		keys := strings.Split(u.Path, "/")[1:3]
 		host = u.Host + "/" + strings.Join(keys, "/")
+	case "www.facebook.com", "facebook.com", "m.facebook.com":
+		host = "facebook.com"
+		keys := strings.Split(u.Path, "/")[1:2]
+		host += "/" + strings.Join(keys, "/")
 	case "pixa.club":
 		keys := strings.Split(u.Path, "/")[1:3]
 		host = u.Host + "/" + strings.Join(keys, "/")
--- host_parser_test.go
+++ host_parser_test.go
@@ -18,14 +18,15 @@ func TestSimpleHostKey(t *testing.T) {
 
 func TestMultipleHostKey(t *testing.T) {
 	for uri, expected := range map[string]string{
-		"http://www.readcomics.tv/the-unbeatable-squirrel-girl/chapter-3/24":  "www.readcomics.tv/the-unbeatable-squirrel-girl",
-		"http://readcomiconline.to/Comic/Jughead-2015/Issue-15?id=112397":     "readcomiconline.to/Comic/Jughead-2015",
-		"https://kimcartoon.to/Cartoon/DuckTales-2017/Episode-1?id=76477":     "kimcartoon.to/Cartoon/DuckTales-2017",
-		"http://pixa.club/en/gravity-falls/season-1/epizod-14-bottomless-pit": "pixa.club/en/gravity-falls",
-		"https://www.youtube.com/watch?v=FkSFBcjOKHY&list=RDFkSFBcjOKHY":      "youtube.com/watch?list=RDFkSFBcjOKHY",
-		"https://m.youtube.com/watch?v=FkSFBcjOKHY&list=RDFkSFBcjOKHY":        "youtube.com/watch?list=RDFkSFBcjOKHY",
-		"https://youtu.be/watch?v=FkSFBcjOKHY&list=RDFkSFBcjOKHY":             "youtube.com/watch?list=RDFkSFBcjOKHY",
-		"https://youtu.be/watch?v=FkSFBcjOKHY":                                "youtube.com/watch?v=FkSFBcjOKHY",
+		"http://www.readcomics.tv/the-unbeatable-squirrel-girl/chapter-3/24":    "www.readcomics.tv/the-unbeatable-squirrel-girl",
+		"http://readcomiconline.to/Comic/Jughead-2015/Issue-15?id=112397":       "readcomiconline.to/Comic/Jughead-2015",
+		"https://kimcartoon.to/Cartoon/DuckTales-2017/Episode-1?id=76477":       "kimcartoon.to/Cartoon/DuckTales-2017",
+		"http://pixa.club/en/gravity-falls/season-1/epizod-14-bottomless-pit":   "pixa.club/en/gravity-falls",
+		"https://www.youtube.com/watch?v=FkSFBcjOKHY&list=RDFkSFBcjOKHY":        "youtube.com/watch?list=RDFkSFBcjOKHY",
+		"https://m.youtube.com/watch?v=FkSFBcjOKHY&list=RDFkSFBcjOKHY":          "youtube.com/watch?list=RDFkSFBcjOKHY",
+		"https://youtu.be/watch?v=FkSFBcjOKHY&list=RDFkSFBcjOKHY":               "youtube.com/watch?list=RDFkSFBcjOKHY",
+		"https://youtu.be/watch?v=FkSFBcjOKHY":                                  "youtube.com/watch?v=FkSFBcjOKHY",
+		"https://www.facebook.com/humandiscoveriesshow/videos/501578117278841/": "facebook.com/humandiscoveriesshow",
 	} {
 
 		u, _ := url.Parse(uri)
